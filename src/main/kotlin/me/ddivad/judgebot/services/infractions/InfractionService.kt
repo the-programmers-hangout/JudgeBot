278111637e31b0c678b0d59da4a331482da14580
ddivad195 <ddivad195@gmail.com>
2020-10-13 16:45:26 +0100

    feat: add logging service
    Added logging service to the bot to log mutes, infractions & rejoin mutes.

--- src/main/kotlin/me/ddivad/judgebot/services/infractions/InfractionService.kt
+++ src/main/kotlin/me/ddivad/judgebot/services/infractions/InfractionService.kt
@@ -7,15 +7,19 @@ import me.ddivad.judgebot.dataclasses.GuildMember
 import me.ddivad.judgebot.dataclasses.Infraction
 import me.ddivad.judgebot.embeds.createInfractionEmbed
 import me.ddivad.judgebot.services.DatabaseService
+import me.ddivad.judgebot.services.LoggingService
 import me.jakejmattson.discordkt.api.annotations.Service
 import me.jakejmattson.discordkt.api.extensions.sendPrivateMessage
 
 @Service
-class InfractionService(private val configuration: Configuration, private val databaseService: DatabaseService) {
+class InfractionService(private val configuration: Configuration,
+                        private val databaseService: DatabaseService,
+                        private val loggingService: LoggingService) {
     suspend fun infract(target: Member, guild: Guild, userRecord: GuildMember, infraction: Infraction): GuildMember {
         target.asUser().sendPrivateMessage {
             createInfractionEmbed()
         }
+        loggingService.infractionApplied(guild, target.asUser(), infraction)
         return databaseService.users.addInfraction(guild, userRecord, infraction)
     }
 }
