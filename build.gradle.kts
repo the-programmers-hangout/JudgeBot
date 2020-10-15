00b598d5961c97d72435392a8f20230fb87704b2
ddivad195 <ddivad195@gmail.com>
2020-10-15 18:43:13 +0100

    feat: add initial docker config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "me.ddivad"
-version = "0.0.1"
+version = Versions.BOT
 description = "judgebot"
 
 plugins {
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.serialization") version "1.4.10"
     id("com.github.ben-manes.versions") version "0.33.0"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 repositories {
@@ -16,11 +17,27 @@ repositories {
 }
 
 dependencies {
-    implementation("me.jakejmattson:DiscordKt:0.20.0")
+    implementation("me.jakejmattson:DiscordKt:${Versions.DISCORDKT}")
     implementation("org.litote.kmongo:kmongo-coroutine:4.1.2")
     implementation("joda-time:joda-time:2.10.6")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    shadowJar {
+        archiveFileName.set("Judgebot.jar")
+        manifest {
+            attributes(
+                    "Main-Class" to "me.ddivad.judgebot.MainKt"
+            )
+        }
+    }
+}
+
+object Versions {
+    const val BOT = "0.0.1"
+    const val DISCORDKT = "0.20.0"
 }
