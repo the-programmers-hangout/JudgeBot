4d2c39807d26f5c2945034400d1a1c2cd5b26021
ddivad195 <ddivad195@gmail.com>
2020-11-10 13:46:29 +0000

    chore: remove unused imports

--- src/main/kotlin/me/ddivad/judgebot/listeners/StaffReactionListeners.kt
+++ src/main/kotlin/me/ddivad/judgebot/listeners/StaffReactionListeners.kt
@@ -1,18 +1,14 @@
 package me.ddivad.judgebot.listeners
 
 import com.gitlab.kordlib.core.event.message.ReactionAddEvent
-import com.gitlab.kordlib.kordx.emoji.Emojis
-import com.gitlab.kordlib.rest.route.Route
 import me.ddivad.judgebot.dataclasses.Configuration
 import me.ddivad.judgebot.embeds.createMessageDeleteEmbed
 import me.ddivad.judgebot.embeds.createSelfHistoryEmbed
 import me.ddivad.judgebot.services.DatabaseService
-import me.ddivad.judgebot.services.LoggingService
 import me.ddivad.judgebot.services.PermissionLevel
 import me.ddivad.judgebot.services.PermissionsService
 import me.ddivad.judgebot.services.infractions.MuteService
 import me.jakejmattson.discordkt.api.dsl.listeners
-import me.jakejmattson.discordkt.api.extensions.isImagePost
 import me.jakejmattson.discordkt.api.extensions.sendPrivateMessage
 
 fun onStaffReactionAdd(muteService: MuteService,
