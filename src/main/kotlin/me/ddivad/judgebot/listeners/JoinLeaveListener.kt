c66ca1607da08fded35d7346cfd0659e5210efe6
ddivad195 <ddivad195@gmail.com>
2020-11-13 13:41:39 +0000

    fix: update join/leave listener logic

--- src/main/kotlin/me/ddivad/judgebot/listeners/JoinLeaveListener.kt
+++ src/main/kotlin/me/ddivad/judgebot/listeners/JoinLeaveListener.kt
@@ -4,6 +4,7 @@ import com.gitlab.kordlib.core.event.guild.GuildCreateEvent
 import com.gitlab.kordlib.core.event.guild.MemberJoinEvent
 import com.gitlab.kordlib.core.event.guild.MemberLeaveEvent
 import com.gitlab.kordlib.gateway.RequestGuildMembers
+import kotlinx.coroutines.flow.toList
 import me.ddivad.judgebot.services.DatabaseService
 import me.ddivad.judgebot.services.LoggingService
 import me.jakejmattson.discordkt.api.dsl.listeners
@@ -15,13 +16,16 @@ fun onGuildMemberLeave(loggingService: LoggingService, databaseService: Database
     }
 
     on<MemberLeaveEvent> {
-        val userRecord = databaseService.users.getOrCreateUser(this.user.asUser(), this.guild.asGuild())
-        databaseService.users.addGuildLeave(userRecord, this.getGuild(), DateTime.now().millis)
+        databaseService.users.getUserOrNull(this.user.asUser(), this.guild.asGuild())?.let {
+            databaseService.users.addGuildLeave(it, guild.asGuild(), DateTime.now().millis)
+        }
     }
 
     on<MemberJoinEvent> {
-        databaseService.users.getUserOrNull(this.member, this.guild.asGuild())?.let {
-            databaseService.users.addGuildJoin(guild.asGuild(), it, this.member.joinedAt.toEpochMilli())
+        databaseService.users.getUserOrNull(this.member.asUser(), this.guild.asGuild())?.let {
+            databaseService.users.addGuildJoin(this.getGuild(), it, this.member.joinedAt.toEpochMilli())
+            return@on
         }
+        databaseService.users.getOrCreateUser(this.member.asUser(), this.getGuild())
     }
 }
