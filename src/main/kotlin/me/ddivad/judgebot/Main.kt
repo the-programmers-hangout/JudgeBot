96cb8c987b130aeb80bfb93d11066ff19a5dfe79
ddivad195 <ddivad195@gmail.com>
2020-12-15 13:31:50 +0000

    chore: bump version for 1.6.0

--- src/main/kotlin/me/ddivad/judgebot/Main.kt
+++ src/main/kotlin/me/ddivad/judgebot/Main.kt
@@ -58,7 +58,7 @@ suspend fun main(args: Array<String>) {
             field {
                 name = "Build Info"
                 value = "```" +
-                        "Version:   1.5.0\n" +
+                        "Version:   1.6.0\n" +
                         "DiscordKt: ${versions.library}\n" +
                         "Kotlin:    $kotlinVersion" +
                         "```"
