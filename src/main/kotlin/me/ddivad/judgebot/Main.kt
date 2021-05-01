83b3207965d07a4ec77f01f29e6c3a13825f0b20
David <ddivad195@gmail.com>
2021-05-01 12:00:57 +0100

    chore: update version

--- src/main/kotlin/me/ddivad/judgebot/Main.kt
+++ src/main/kotlin/me/ddivad/judgebot/Main.kt
@@ -58,7 +58,7 @@ suspend fun main(args: Array<String>) {
             field {
                 name = "Build Info"
                 value = "```" +
-                        "Version:   1.6.1\n" +
+                        "Version:   1.6.2\n" +
                         "DiscordKt: ${versions.library}\n" +
                         "Kotlin:    $kotlinVersion" +
                         "```"
