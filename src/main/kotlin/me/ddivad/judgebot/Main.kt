cbebca541ba6964b9dd188f2f5205dd67a7aac82
David <ddivad195@gmail.com>
2021-06-01 15:13:59 +0100

    release: bump version for 1.8.2 release

--- src/main/kotlin/me/ddivad/judgebot/Main.kt
+++ src/main/kotlin/me/ddivad/judgebot/Main.kt
@@ -58,7 +58,7 @@ suspend fun main(args: Array<String>) {
             field {
                 name = "Build Info"
                 value = "```" +
-                        "Version:   1.8.1\n" +
+                        "Version:   1.8.2\n" +
                         "DiscordKt: ${versions.library}\n" +
                         "Kotlin:    $kotlinVersion" +
                         "```"
