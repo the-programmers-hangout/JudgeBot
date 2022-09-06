438b7a2ba51714780c3bfa378cd4f65f8aaf0138
ddivad195 <ddivad195@gmail.com>
2022-09-06 23:20:33 +0100

    feat: v1 migration cleanup

--- src/main/kotlin/me/ddivad/judgebot/services/database/migrations/v1.kt
+++ src/main/kotlin/me/ddivad/judgebot/services/database/migrations/v1.kt
@@ -1,10 +1,15 @@
 package me.ddivad.judgebot.services.database.migrations
 
+import me.ddivad.judgebot.services.database.GuildOperations
+import me.ddivad.judgebot.services.database.JoinLeaveOperations
+import me.ddivad.judgebot.services.database.MessageDeleteOperations
+import me.ddivad.judgebot.services.database.UserOperations
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 suspend fun v1(db: CoroutineDatabase) {
     println("Running v1 DB Migration")
-    db.createCollection("Guilds")
-    db.createCollection("JoinLeaves")
-    db.createCollection("Users")
+    db.createCollection(GuildOperations.name)
+    db.createCollection(JoinLeaveOperations.name)
+    db.createCollection(UserOperations.name)
+    db.createCollection(MessageDeleteOperations.name)
 }
