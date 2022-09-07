63bfb1d30eb02ab4e71c1768a2c8adab1f8c2b49
ddivad195 <ddivad195@gmail.com>
2022-09-07 12:33:39 +0100

    feat: migration updates, change to use bulk writes

--- src/main/kotlin/me/ddivad/judgebot/services/database/migrations/v2.kt
+++ src/main/kotlin/me/ddivad/judgebot/services/database/migrations/v2.kt
@@ -1,33 +1,44 @@
 package me.ddivad.judgebot.services.database.migrations
 
+import com.mongodb.client.model.ReplaceOneModel
+import me.ddivad.judgebot.dataclasses.Ban
 import me.ddivad.judgebot.dataclasses.GuildInformation
 import me.ddivad.judgebot.dataclasses.GuildMember
 import me.ddivad.judgebot.services.database.GuildOperations
 import me.ddivad.judgebot.services.database.UserOperations
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.eq
+import org.litote.kmongo.replaceOne
+
+data class Result(val guildId: String, val bans: List<Ban>)
 
 suspend fun v2(db: CoroutineDatabase) {
     println("Running v2 DB Migration")
     val userCollection = db.getCollection<GuildMember>(UserOperations.name)
     val guildCollection = db.getCollection<GuildInformation>(GuildOperations.name)
+    val guildBans = guildCollection.find().toList().map { Result(it.guildId, it.bans) }
 
+    val banDocuments = mutableListOf<ReplaceOneModel<GuildInformation>>()
     guildCollection.find().consumeEach { guild ->
         guild.bans.forEach {
             it.thinIce = false
             it.unbanTime = null
             it.dateTime = null
         }
-        guildCollection.updateOne(GuildInformation::guildId eq guild.guildId, guild)
+        banDocuments.add(replaceOne(GuildInformation::guildId eq guild.guildId, guild))
     }
+    guildCollection.bulkWrite(requests = banDocuments)
 
-    guildCollection.find().consumeEach {
-        it.bans.forEach { ban ->
-            val user = userCollection.findOne(GuildMember::userId eq ban.userId)
-            if (user != null) {
-                user.guilds.find { g -> g.guildId == it.guildId }?.bans?.add(ban)
-                userCollection.updateOne(GuildMember::userId eq user.userId, user)
+    val userDocuments = mutableListOf<ReplaceOneModel<GuildMember>>()
+    userCollection.find().consumeEach { user ->
+        guildBans.forEach { gb ->
+            val userBan = gb.bans.find { it.userId == user.userId }
+            if (userBan != null) {
+                user.guilds.find { it.guildId == gb.guildId }?.bans?.add(userBan)
             }
         }
+        user.guilds.forEach { guildDetails -> guildDetails.pointDecayFrozen = false }
+        userDocuments.add(replaceOne(GuildMember::userId eq user.userId, user))
     }
+    userCollection.bulkWrite(requests = userDocuments)
 }
