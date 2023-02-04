59df6570be1b5a199548fcd8251e75c411bf8224
ddivad195 <ddivad195@gmail.com>
2023-02-04 22:12:52 +0000

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.*
 
 group = "me.ddivad"
-version = "2.0.0-RC1"
+version = "2.9.0"
 description = "A bot for managing discord infractions in an intelligent and user-friendly way."
 
 plugins {
